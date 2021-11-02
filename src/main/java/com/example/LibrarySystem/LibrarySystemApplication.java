ce5439254abf5eeee5393aa320126de9705c74b4
sudarshankannan <sudarshan.kannan99@gmail.com>
2021-11-01 22:33:20 -0700

    added indexcontroller

--- src/main/java/com/example/LibrarySystem/LibrarySystemApplication.java
+++ src/main/java/com/example/LibrarySystem/LibrarySystemApplication.java
@@ -2,6 +2,9 @@ package com.example.LibrarySystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LibrarySystemApplication {
@@ -9,5 +12,4 @@ public class LibrarySystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibrarySystemApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/controllers/IndexController.java
+++ src/main/java/com/example/controllers/IndexController.java
@@ -5,11 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-public class HelloController {
-
+public class IndexController {
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "index";
 	}
 
 }
